881d0dcd131f1ada41dbe7f98bf8ffea016f7b77
emad22 <emadalhamwi34@gmail.com>
2024-01-03 11:36:47 -0400

    update

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-7p$35*10gz2&5rmz1ny$7)#7!urc@1przgjw1kh#i!t&6e%(s2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['hiretrail.azurewebsites.net']
 
@@ -150,3 +150,34 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 #     'django.contrib.auth.backends.ModelBackend',
 # ]
 
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+CSRF_TRUSTED_ORIGIN =['https://'+os.environ['WEBSITE_HOSTNAME']]
+DEBUG = False
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+connection_string= os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split('')}
+
+DATABASES={
+
+    'defualt': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': parameters['dbname'],
+        'HOST': parameters['host'],
+
+        'USER': parameters['user'],
+        'PASSWORD': parameters['password'],
+    }
+}
