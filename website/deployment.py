748f844edfc5b9d95e5851d23e38e1628ec4743d
emad22 <emadalhamwi34@gmail.com>
2024-01-03 10:50:57 -0400

    COM

--- website/deployment.py
+++ website/deployment.py
@@ -1,6 +1,7 @@
 import os
 from .settings import *
 from .settings import BASE_DIR
+SECRET_KEY = os.environ['SECRET']
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGIN =['https://'+os.environ['WEBSITE_HOSTNAME']]
